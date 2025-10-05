506d8099cb080e44dac2891571d92743aadb3fae
tonistonissoo-admin <toniston@ut.ee>
2025-10-05 15:36:06 +0300

    ui

--- Assets/Scripts/keyslot.cs
+++ Assets/Scripts/keyslot.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class keyslot : MonoBehaviour
@@ -6,35 +7,60 @@ public class keyslot : MonoBehaviour
     private int index = 0;          
     public bool lockpicks_aligned = true;
 
+    [Header("UI")]
+    [SerializeField] private TMP_Text endText;    // Drag TMP tekst siia
+    [SerializeField] private GameObject endPanel;
+
     void Update()
     {
-        if (index<lockpicks.Length){
+        if (index < lockpicks.Length)
+        {
             if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    if (index < lockpicks.Length)
-                    {
-                        
-                        lockpicks[index].GetComponent<movement>().stop_pick();
-                        index++;
-                    }
-                }
-        
-            }
-        else if (index == lockpicks.Length)
             {
-                foreach (GameObject el in lockpicks)
-                {
-                    if (el.GetComponent<movement>().lock_open == false)
-                    {
-                        lockpicks_aligned = false;
-                    }
-                }
-                if (lockpicks_aligned)
-                {
-                    Debug.Log("Good Job you opened the lock.");
-                }
+                lockpicks[index].GetComponent<movement>().stop_pick();
                 index++;
-                
             }
+        }
+        else if (index == lockpicks.Length)
+        {
+            lockpicks_aligned = true; // reset kontrolliks
+
+            foreach (GameObject el in lockpicks)
+            {
+                if (el.GetComponent<movement>().lock_open == false)
+                {
+                    lockpicks_aligned = false;
+                    break;
+                }
+            }
+
+            if (lockpicks_aligned)
+            {
+                ShowEndMessage("You won 1 000 000 coins!");
+            }
+            else
+            {
+                ShowEndMessage("Unlucky");
+            }
+
+            index++;
+        }
+    }
+
+    private void ShowEndMessage(string message)
+    {
+        if (endText != null)
+        {
+            endText.text = message;
+            endText.gameObject.SetActive(true);
+        }
+
+        if (endPanel != null)
+        {
+            endPanel.SetActive(true);
+        }
+
+        Debug.Log(message);
+        Time.timeScale = 0f;
     }
 }
